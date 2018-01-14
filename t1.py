cac3ac16c04e13149d20f70b8b1e9609f36e9b5a
Ilya.Shadrin <ilshadrin@mail.ru>
2018-01-14 19:13:49 +0300

    дополнена диагностика и конфигурация стат маршрутов, интерфейсов и оспф

--- t1.py
+++ t1.py
@@ -14,7 +14,9 @@ my_flask_app = Flask(__name__)
 
 @my_flask_app.route('/')
 def input(): 
-    return render_template('show_date.html')
+    return render_template('web_morda.html')
+
+
 
 #ПРОВЕРКА КОРРЕКТНОСТИ ВВЕДЕННЫХ ЛОГИНА, ПАРОЛЯ И IP
 @my_flask_app.route('/correct')
@@ -22,105 +24,148 @@ def correct():
     ip_adress = request.args.get('ip_adress', 0, type=str)
     login = request.args.get('login', 0, type=str)
     password = request.args.get('password', 0, type=str)
-
- 
-    #itog=ip_adress, login,  password
-    print(ip_adress)
-    print(login)
-    print(password)
-
     telnet = telnetlib.Telnet(ip_adress)
     telnet.read_until(b"Username:")
     telnet.write(login.encode('utf-8')+ b'\n')
     telnet.read_until(b"Password: ")
     telnet.write(password.encode('utf-8')+ b'\n')
     time.sleep(0.5)
-
-    print('успешно залогинились')
-
     itog = telnet .read_very_eager().decode('utf-8')
-    print(itog)
-    
-    print('itog.find', itog.find('#'))
     itog2 =' '
     if itog.find('#')>0:
         itog2 ='успешно'
     else:
-        itog2 ='не успешно'
-
-    print('itog2', itog2)
-
-      
+        itog2 ='не успешно'      
     return jsonify(result_correct=itog2)
 
 
-#ВВОД КОМАНДЫ ДЛЯ МАРШРУТИЗАТОРА
-@my_flask_app .route('/command')
-def command():
 
-    command = request.args.get('command', 0, type=str).encode('utf-8')
-    print('команда 2 ', command )
-    res=' '
-
-    ip_adress = request.args.get('ip_adress', 0,  type=str)
-    login = request.args.get('login', 0, type=str)
-    password = request.args.get('password', 0,  type=str)
-
-
-    print('ip_adress ', ip_adress)
-    print('login  ' , login)
-    print('password ', password)
-
-    telnet = telnetlib.Telnet(ip_adress)
-
-    #telnet.set_debuglevel(6) #дебаг
-
-    telnet.read_until(b"Username:")
-    telnet.write(login.encode('utf-8')+ b'\n')
-    telnet.read_until(b"Password: ")
-    telnet.write(password.encode('utf-8')+ b'\n')
-    time.sleep(0.5)
-
-    print('успешно залогинились')
-
-    telnet.write(command + b'\n')
-    time.sleep(0.5)
-    time.sleep(0.5)
-    print('ввели команду')
-    res = telnet .read_very_eager().decode('utf-8')
-    print('вывод команды', res)
-
-
-    return jsonify(result_command = res)
-
-
-
-
-
-#ВВОД command_run всех команд по кнопкам
+#ВВОД command_run  по кнопкам
 @my_flask_app .route('/command_run')
 def command_run():
-    res_command_run=' '
-
-    command_run= request.args.get('command_run', 0, type=str).encode('utf-8')
+    
+    command_run = request.args.get('command_run', 0, type=str).encode('utf-8')
     ip_adress = request.args.get('ip_adress', 0,  type=str)
     login = request.args.get('login', 0, type=str)
     password = request.args.get('password', 0,  type=str)
 
+    ip_adress_ping_trace = request.args.get('ip_adress_ping_trace', 0, type=str).encode('utf-8')
+
 
     telnet = telnetlib.Telnet(ip_adress)
-
     telnet.read_until(b"Username:")
     telnet.write(login.encode('utf-8')+ b'\n')
     telnet.read_until(b"Password: ")
     telnet.write(password.encode('utf-8')+ b'\n')
-    time.sleep(0.5)  
-    telnet.write(command_run + b'\n')
     time.sleep(0.5)
+
+#обработка ping и trace
+
+    if request.args.get('command_run', 0, type=str) in ('ping', 'trace'):
+        print('я тут')
+        print(command_run  + b' ' + ip_adress_ping_trace + b'\n')
+
+        telnet.write(command_run  + b' ' + ip_adress_ping_trace + b'\n')
+        time.sleep(0.5)
+
+#обработка остальных команд
+
+    else:
+        telnet.write(command_run + b'\n')
+        time.sleep(0.5)
+
     res_command_run= telnet .read_very_eager().decode('utf-8')
     return jsonify(result_command_run = res_command_run)
 
 
+
+ #конфигурационный режим
+
+@my_flask_app .route('/configure_mode')
+def configure_mode():
+
+
+#получение исходных данных
+
+
+    configure_command = request.args.get('configure_command', 0,  type=str)
+
+    print('НАИМЕНОВАНИЕ КОМАНДЫ!!!!!!!!!', configure_command)
+
+    ip_adress = request.args.get('ip_adress', 0,  type=str)
+    login = request.args.get('login', 0, type=str)
+    password = request.args.get('password', 0,  type=str)
+
+#начальные данные для конфигурации интерфейса
+    interface_name= request.args.get('interface_name', 0,  type=str).encode('utf-8')
+    interface_ip_adress = request.args.get('interface_ip_adress', 0, type=str).encode('utf-8')
+    interface_mask = request.args.get('interface_mask', 0, type=str).encode('utf-8')
+
+#начальные данные для конфигурации маршрутов
+    network_ip_adress= request.args.get('network_ip_adress', 0,  type=str).encode('utf-8')
+    network_mask = request.args.get('network_mask', 0, type=str).encode('utf-8')
+    next_hop_ip = request.args.get('next_hop_ip', 0, type=str).encode('utf-8')
+
+
+#начальные данные для конфигурации ospf
+
+    ospf_process_id= request.args.get('ospf_process_id', 0,  type=str).encode('utf-8')
+    ospf_ip_adress_interface = request.args.get('ospf_ip_adress_interface', 0, type=str).encode('utf-8')
+    ospf_mask= request.args.get('ospf_mask', 0, type=str).encode('utf-8')
+    ospf_area= request.args.get('ospf_area', 0, type=str).encode('utf-8')
+    
+    
+#вход на роутер и в  конфигурационный режим
+
+
+    telnet = telnetlib.Telnet(ip_adress)
+    telnet.read_until(b"Username:")
+    telnet.write(login.encode('utf-8')+ b'\n')
+    telnet.read_until(b"Password: ")
+    telnet.write(password.encode('utf-8')+ b'\n')
+    time.sleep(0.5)
+    telnet.write(b'conf t\n')
+    time.sleep(0.5)
+
+
+
+    if configure_command=='configure_interface':
+        print('создаем интерфейс')
+#создаем интерфейс
+        telnet.write(b'interface ' + interface_name  + b'\n')
+        time.sleep(0.5)
+        telnet.write(b'ip add '  + b' ' + interface_ip_adress + b' ' + interface_mask + b'\n')
+        time.sleep(0.5)
+
+    elif configure_command=='configure_route':
+        print('пописываем маршрут')
+
+#прописываем маршурты
+        telnet.write(b'ip route ' + network_ip_adress + b' '+ network_mask + b' ' +  next_hop_ip  + b'\n')
+        time.sleep(0.5)
+
+
+# запускаем ospf
+    elif configure_command=='configure_ospf':
+
+        telnet.write(b'router ospf ' + ospf_process_id + b'\n')
+        time.sleep(0.5)
+        telnet.write(b'network ' + ospf_ip_adress_interface + b' ' + ospf_mask + b' area ' + ospf_area + b'\n')
+        time.sleep(0.5)
+
+
+
+
+
+    res_configure_mode = telnet .read_very_eager().decode('utf-8')
+    return jsonify(result_configure_mode = res_configure_mode)
+
+
+    
+  
+    
+    
+    
 
 
 if __name__=="__main__":
--- telnet_itog.py
+++ telnet_itog.py
@@ -17,11 +17,16 @@ telnet.read_until(b"Password: ")
 telnet.write(password.encode('utf-8')+ b'\n')
 time.sleep(0.5)
 
+
+
+
+
+'''
 telnet.write(b'conf t\n')
 time.sleep(0.5)
 telnet.write(b'interface loopback 0\n')
 time.sleep(0.5)
 telnet.write(b'ip add 8.8.8.8 255.255.255.255\n')
 time.sleep(0.5)
-
+'''
 
